1c93ba81588b73c17d3b6b53cb019948f6419811
Bruno Pastorino <108767190+Bexzy@users.noreply.github.com>
2022-11-24 20:39:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 const JSONlink = "http://localhost:3000";
-let data = await getJSON(JSONlink);
+
+
 
 async function getJSON(link) {
     const rawdata = await fetch(link);
-    console.log(rawdata);
     const data = await rawdata.json();
     console.log(data);
+    return data
 }
 
 
 document.addEventListener("DOMContentLoaded", async ()=> {
+    let data = await getJSON(JSONlink);
 
+
+    data.forEach(element => {
+        document.getElementById("info").innerHTML += 
+        `
+        <div class="mb-2 fw-bold bg-light" style="color: black;">
+        ID: ${element.id} <br>
+        Nombre: ${element.first_name} <br>
+        Apellido: ${element.last_name} <br>
+        Género: ${element.gender} <br>
+        Email: ${element.email} <br>
+        </div>
+        `
+    });
 })
 
 const btnSearch = document.getElementById("btnSearch")
